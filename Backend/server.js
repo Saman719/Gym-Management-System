35e5b5a503e426cfced0f04954d50abb2bff441c
Saman Goudarzi <sgoudarzi79@gmail.com>
2023-07-05 16:17:35 +0330

    completed users APIs

--- Backend/server.js
+++ Backend/server.js
@@ -7,21 +7,6 @@ const users = require('./users');
 const { Sequelize, DataTypes } = require('sequelize');
 const User = require('./Model/users-model');
 
-const my_user = User.create({
-    username: 'example_user',
-    pass: 'example_password',
-    name: 'John',
-    familyName: 'Doe',
-    shiftStart: '08:00',
-    shiftEnd: '16:00'
-  });
-console.log(my_user)
-// use the User model to interact with the database
-User.findAll().then(users => {
-  console.log(users);
-});
-// const courses = require('./courses');
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -50,19 +35,19 @@ app.post('/login', async (req, res) => {
     // Retrieve the user from the database.
     // Replace with your actual database query.
     const user = await getUserFromDatabase(username);
-    
+
     if (!user) {
         // Handle case when user is not found
         return res.status(401).send('Unauthorized');
     }
-    
+
     const passwordValid = await bcrypt.compare(password, user.password);
-    
+
     if (!passwordValid) {
         // Handle case when password is not valid
         return res.status(401).send('Unauthorized');
     }
-    
+
     // User is authenticated, create a JWT.
     const token = jwt.sign({ userId: user.id }, 'your_secret_key');
 
--- Backend/users.js
+++ Backend/users.js
@@ -1,12 +1,130 @@
 const express = require('express');
 const router = express.Router();
+const User = require('./Model/users-model');
+const bcrypt = require('bcryptjs');
 
-router.get('/register', (req, res) => {
-  console.log(req)
+router.post('/register', async (req, res) => {
+    const { username, password, name, familyName, shiftStart, shiftEnd, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+        username,
+        password: hashedPassword,
+        name,
+        familyName,
+        shiftStart,
+        shiftEnd,
+        role
+    };
+    try {
+        const user = await User.create(newUser);
+        return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        return res.status(500).json({
+            errorName: error.name,
+            errorMessage: error.message
+        });
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // Handle request for a specific course
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+        return res.status(200).json(user);
+    } else {
+        return res.status(401).json({ error: 'Invalid username or password' });
+    }
+});
+
+router.delete('/delete', async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        const result = await User.destroy({ where: { username } });
+        res.json({ success: result == 0 ? false : true, message: `Deleted ${result} user(s)` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed to delete user' });
+    }
+});
+
+router.get('/all', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed to fetch users' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ success: false, message: 'User not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed to fetch user' });
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { username, password, name, familyName, shiftStart, shiftEnd, role } = req.body;
+
+    try {
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+
+        if (username) {
+            user.username = username;
+        }
+        if (password) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            user.password = hashedPassword;
+        }
+        if (name) {
+            user.name = name;
+        }
+        if (familyName) {
+            user.familyName = familyName;
+        }
+        if (shiftStart) {
+            user.shiftStart = shiftStart;
+        }
+        if (shiftEnd) {
+            user.shiftEnd = shiftEnd;
+        }
+        if (role) {
+            user.role = role;
+        }
+
+        // Save the updated user to the database
+        await user.save();
+
+        res.json({ success: true, message: 'User updated successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Failed to update user' });
+    }
 });
 
 module.exports = router;
